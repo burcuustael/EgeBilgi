fcac9f949b579bd7c9f70f9d41613690795422a9
burcuustael <burcustael@gmail.com>
2024-09-27 16:34:54 +0300

    pagination tamamlandı.

--- src/app/ui/layout/main/character-list-component/character-list-component.component.ts
+++ src/app/ui/layout/main/character-list-component/character-list-component.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CharacterService } from '../services/character.service';
 import { Character } from '../model/character.model';
-import { forkJoin } from 'rxjs';
-
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { CharacterPipe } from '../../../pipes/chracter.pipe';
 import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-character-list-component',
   standalone: true,
-  imports: [HttpClientModule, CommonModule, CharacterPipe, FormsModule],
+  imports: [
+    HttpClientModule,
+    CommonModule,
+    CharacterPipe,
+    FormsModule,
+    NgxPaginationModule,
+  ],
   templateUrl: './character-list-component.component.html',
   styleUrl: './character-list-component.component.css',
 })
@@ -31,49 +36,13 @@ export class CharacterListComponentComponent implements OnInit {
 
   loadCharacters(): void {
     this.characterService.getAllCharacters().subscribe((data) => {
+      this.characters = data.results;
       this.totalCharacters = data.info.count;
-      this.fetchPage(this.currentPage);
       this.filteredCharacters = this.characters;
     });
   }
 
-  fetchPage(page: number): void {
-    const startIndex = (page - 1) * this.pageSize;
-    const endIndex = Math.min(startIndex + this.pageSize, this.totalCharacters);
-
-    const randomIds = this.characterService.getRandomCharacterIds(
-      this.pageSize
-    );
-
-    const requests = randomIds.map((id) =>
-      this.characterService.getCharacter(id)
-    );
-
-    forkJoin(requests).subscribe(
-      (results: Character[]) => {
-        this.characters = results;
-      },
-      (error) => {
-        console.error('Hata oluştu:', error);
-      }
-    );
-  }
-  nextPage(): void {
-    if (this.currentPage * this.pageSize < this.totalCharacters) {
-      this.currentPage++;
-      this.fetchPage(this.currentPage);
-    }
-  }
-
-  prevPage(): void {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.fetchPage(this.currentPage);
-    }
-  }
-  onSearchChange() {
-    this.filteredCharacters = this.characters.filter((character) =>
-      character.name.toLowerCase().includes(this.search.toLowerCase())
-    );
+  pageChanged(event: number): void {
+    this.currentPage = event;
   }
 }
